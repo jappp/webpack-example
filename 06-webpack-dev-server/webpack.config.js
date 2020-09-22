f860988249576baacbe8563de2e23b31499a460d
pengzhaorui <jappp951@163.com>
2020-09-22 19:03:49 +0800

    webpack-dev-server

---
+++ 06-webpack-dev-server/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+  // ...
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    proxy: {
+      '/api': {
+        target: 'https://api.github.com',
+        pathRewrite: {
+          '^/api': '' // 替换掉代理地址中的 /api
+        },
+        changeOrigin: true // 确保请求 GitHub 的主机名就是：api.github.com
+      }
+    }
+  }
+}
