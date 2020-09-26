4dec9ed0a84606e223962e28a9c95d59a2f7b201
pzr <jappp951@163.com>
2020-09-26 21:16:59 +0800

    hmr

---
+++ 08-hmr/src/main.js
@@ -0,0 +1,23 @@
+import createEditor from './editor'
+
+const editor = createEditor()
+document.body.appendChild(editor)
+
+// ... 原本的业务代码
+
+// HMR --------------------------------
+let lastEditor = editor
+module.hot.accept('./editor', () => {
+  // 当 editor.js 更新，自动执行此函数
+  // 临时记录更新前编辑器内容
+  const value = lastEditor.innerHTML
+  // 移除更新前的元素
+  document.body.removeChild(lastEditor)
+  // 创建新的编辑器
+  // 此时 createEditor 已经是更新过后的函数了
+  lastEditor = createEditor()
+  // 还原编辑器内容
+  lastEditor.innerHTML = value
+  // 追加到页面
+  document.body.appendChild(lastEditor)
+})
---
+++ 08-hmr/webpack.config.js
@@ -0,0 +1,17 @@
+const webpack = require('webpack')
+
+module.exports = {
+  // ...
+  entry: './src/main.js',
+  devServer: {
+    // 开启 HMR 特性，如果资源不支持 HMR 会 fallback 到 live reloading
+    hot: true
+    // 只使用 HMR，不会 fallback 到 live reloading
+    // hotOnly: true
+  },
+  plugins: [
+    // ...
+    // HMR 特性所需要的插件
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
